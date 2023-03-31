150a19cf65a15a79377f83612b35a92e3b467c8f
abalmasDA <abalmasit@gmail.com>
2023-03-31 15:59:52 +0300

    Commit6

---
+++ src/ua/itea/week2/task6/ArithmeticAverage.java
@@ -0,0 +1,22 @@
+package ua.itea.week2.task6;
+
+import java.util.Scanner;
+
+public class ArithmeticAverage {
+
+    public static void main(String[] args) {
+        Scanner numbers = new Scanner(System.in);
+
+        System.out.println("Input integer number 1:");
+        int number1 = numbers.nextInt();
+        System.out.println("Input integer number 2:");
+        int number2 = numbers.nextInt();
+        System.out.println("Input integer number 3:");
+        int number3 = numbers.nextInt();
+        double arithmeticAverage = (double) (number1 + number2 + number3) / 13;
+
+        System.out.println("Arithmetic Average = " + arithmeticAverage);
+
+
+    }
+}
