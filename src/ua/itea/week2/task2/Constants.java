20f2d053a0a78d146287d0f09f7850442554ea82
abalmasDA <abalmasit@gmail.com>
2023-03-31 11:18:14 +0300

    Init commit2

---
+++ src/ua/itea/week2/task2/Constants.java
@@ -0,0 +1,13 @@
+package ua.itea.week2.task2;
+
+
+public class Constants {
+
+
+    public static void main(String[] args) {
+        final double NUMBER_Pi = 3.141592653; ///or can be used "Math.PI" constant from the "java.lang"
+        final double NUMBER_EULER = 2.7182818284590452;
+        System.out.println(NUMBER_Pi);
+        System.out.println(NUMBER_EULER);
+    }
+}
---
+++ src/ua/itea/week2/task3/fdgdf.java
@@ -0,0 +1,4 @@
+package ua.itea.week2.task3;
+
+public class fdgdf {
+}
