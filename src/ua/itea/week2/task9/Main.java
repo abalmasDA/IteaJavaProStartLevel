d9992b4229ccaf7c6df6c23f01521c82daa912e2
abalmasDA <abalmasit@gmail.com>
2023-03-31 16:38:31 +0300

    Commit10

---
+++ src/ua/itea/week2/task9/Main.java
@@ -0,0 +1,16 @@
+package ua.itea.week2.task9;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int uberflu? = 10;
+        int _Identifier = 10;
+        int \Identifier = 10;
+        int &myVar = 10;
+        int myVariab1le = 10;
+
+
+    }
+
+
+}
