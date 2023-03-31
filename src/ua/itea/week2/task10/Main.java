64867697fb76031f2dc7e204accb370b13eb8017
abalmasDA <abalmasit@gmail.com>
2023-03-31 16:56:56 +0300

    Commit11

---
+++ src/ua/itea/week2/task10/Main.java
@@ -0,0 +1,21 @@
+package ua.itea.week2.task10;
+
+public class Main {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+        System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println(a * b);
+        System.out.println(a / b);
+        System.out.println(a % b);
+        System.out.println(a == b);
+        System.out.println(a != b);
+        System.out.println(a > b);
+        System.out.println(a < b);
+        System.out.println(a >= b);
+        System.out.println(a <= b);
+    }
+
+
+}
--- src/ua/itea/week2/task9/Main.java
+++ src/ua/itea/week2/task9/Main.java
@@ -3,11 +3,11 @@ package ua.itea.week2.task9;
 public class Main {
     public static void main(String[] args) {
 
-        int uberflu? = 10;
-        int _Identifier = 10;
-        int \Identifier = 10;
-        int &myVar = 10;
-        int myVariab1le = 10;
+//        int uberflu? = 10;
+//        int _Identifier = 10;
+//        int \Identifier = 10;
+//        int &myVar = 10;
+        int myVariabile = 10;
 
 
     }
