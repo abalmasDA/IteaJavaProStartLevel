c851b597f07dfc766ca00e484c7e6758783bbec0
abalmasDA <abalmasit@gmail.com>
2023-03-31 15:07:38 +0300

    Commit4

---
+++ src/ua/itea/week2/task4/Char.java
@@ -0,0 +1,28 @@
+package ua.itea.week2.task4;
+
+public class Char {
+
+    public static void main(String[] args) {
+        char H = '\u0048';
+        char e = '\u0065';
+        char l = '\u006C';
+        char o = '\u006F';
+        char comma = '\u002C';
+        char D = '\u0044';
+        char m = '\u006D';
+        char y = '\u0079';
+        char t = '\u0074';
+        char r = '\u0072';
+        char A = '\u0041';
+        char b = '\u0062';
+        char a = '\u0061';
+        char s = '\u0073';
+        char exclamationMark = '\u0021';
+
+
+        String greetingPhrase = String.valueOf("" + H + e + l + l + o + comma + " " + D + m + y + t + r + o + " " + A + b + a + l + m + a + s + exclamationMark);
+        System.out.println(greetingPhrase);
+
+
+    }
+}
