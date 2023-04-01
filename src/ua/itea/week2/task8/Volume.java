5150dd7686c7b0e9eac17c6b0d41a46f123bd5b7
abalmasDA <abalmasit@gmail.com>
2023-04-01 16:38:02 +0300

    Commit12

--- src/ua/itea/week2/task8/Volume.java
+++ src/ua/itea/week2/task8/Volume.java
@@ -1,19 +1,27 @@
 package ua.itea.week2.task8;
-
 import java.util.Scanner;
-
 public class Volume {
-
     public static void main(String[] args) {
-        Scanner Scaner = new Scanner(System.in);
+        Scanner Scanner = new Scanner(System.in);
 
-        System.out.println("Input radius of cylinder:");
-        double radius = Scaner.nextDouble();
-        System.out.println("Input height of cylinder:");
-        double height = Scaner.nextDouble();
+        double radius;
+        double height;
+        System.out.println("Input radius and height of cylinder (in format double):");
+              while (true) {
+            if (Scanner.hasNextDouble()) {
+                radius = Scanner.nextDouble();
+                height = Scanner.nextDouble();
+                break;
+            } else {
+                Scanner.next();
+                System.out.print("Your enter wrong format, try again:");
+            }
+        }
+
         double areaSurfaceCylinder = 2 * Math.PI * radius * (radius + height);
-        double VolumeCylinder = Math.PI * Math.pow(radius, 2) * height;
+        double volumeCylinder = Math.PI * Math.pow(radius, 2) * height;
+
         System.out.println("areaSurfaceCylinder = " + areaSurfaceCylinder);
-        System.out.println("VolumeCylinder = " + VolumeCylinder);
+        System.out.println("volumeCylinder = " + volumeCylinder);
     }
 }
