92c9bd47620c88637eedcff37e0edeef5d772eea
abalmasDA <abalmasit@gmail.com>
2023-04-13 21:19:39 +0300

    Commit13

--- src/ua/itea/week2/task8/Volume.java
+++ src/ua/itea/week2/task8/Volume.java
@@ -1,27 +1,41 @@
 package ua.itea.week2.task8;
+
 import java.util.Scanner;
+
 public class Volume {
     public static void main(String[] args) {
-        Scanner Scanner = new Scanner(System.in);
 
-        double radius;
-        double height;
-        System.out.println("Input radius and height of cylinder (in format double):");
-              while (true) {
-            if (Scanner.hasNextDouble()) {
-                radius = Scanner.nextDouble();
-                height = Scanner.nextDouble();
-                break;
-            } else {
-                Scanner.next();
-                System.out.print("Your enter wrong format, try again:");
-            }
-        }
 
-        double areaSurfaceCylinder = 2 * Math.PI * radius * (radius + height);
-        double volumeCylinder = Math.PI * Math.pow(radius, 2) * height;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(scanner.locale());
+//        Scanner scanner = new Scanner(System.in);
 
-        System.out.println("areaSurfaceCylinder = " + areaSurfaceCylinder);
-        System.out.println("volumeCylinder = " + volumeCylinder);
+//        double radius;
+////        double a = 1;
+////        double b = 2d;
+////        double c = 2.0;
+////        double d = 1 + 3.0 ;
+//        double height;
+//        System.out.println("Input radius and height of cylinder (in format double):");
+//        while (true) {
+//            if (scanner.hasNextDouble()) {
+//                radius = scanner.nextDouble();
+//                height = scanner.nextDouble();
+//                break;
+//            } else {
+//                scanner.next();
+//                System.out.print("Your enter wrong format, try again:");
+//            }
+//        }
+//
+//        double areaSurfaceCylinder = 2 * Math.PI * radius * (radius + height);
+//        double volumeCylinder = Math.PI * Math.pow(radius, 2) * height;
+//
+//        System.out.println("areaSurfaceCylinder = " + areaSurfaceCylinder);
+//        System.out.println("volumeCylinder = " + volumeCylinder);
+//        System.out.println(a);
+//        System.out.println(b);
+//        System.out.println(c);
+//        System.out.println(d);
     }
 }
