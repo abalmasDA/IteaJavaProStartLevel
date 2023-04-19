684936271a45c215c19d4cd0d42441879e29b815
abalmasDA <abalmasit@gmail.com>
2023-04-19 13:58:32 +0300

    Commit17

---
+++ src/ua/itea/SumMin.java
@@ -0,0 +1,26 @@
+package ua.itea;
+
+public class SumMin {
+
+    public static void main(String[] args) {
+        int a = 7;
+        int b = 15;
+        int sum = 0;
+
+        for (int i = (a + 1); i < b; i++) {
+            sum += i;
+        }
+        System.out.println("Sum of numbers: " + sum);
+
+
+        for (int i = a + 1; i < b; i++) {
+
+            if (i % 2 != 0) {
+                System.out.println(i);
+            }
+
+        }
+    }
+
+
+}
