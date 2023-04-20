923236ffb98744cf43e14ac2bf80dac4e9dc79f9
abalmasDA <abalmasit@gmail.com>
2023-04-20 19:52:40 +0300

    Commit22

---
+++ src/MultiplicationTable.java
@@ -0,0 +1,16 @@
+public class MultiplicationTable {
+
+
+    public static void main(String[] args) {
+
+        int a = 7;
+        int multiplication = 0;
+
+        for (int i = 1; i <= 10; i++) {
+            multiplication = a * i;
+            System.out.println(a + " * " + i + " = " + multiplication + ";");
+
+        }
+
+    }
+}
