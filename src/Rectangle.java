905ecd1cf4588f809d0b4faa6e3b99ac91e10c58
abalmasDA <abalmasit@gmail.com>
2023-04-23 10:54:43 +0300

    Commit24

---
+++ src/Rectangle.java
@@ -0,0 +1,35 @@
+public class Rectangle {
+
+    public static void main(String[] args) {
+
+        //Output filled rectangle
+        int rectangleHeight1 = 5;
+        int rectangleWidth1 = 10;
+        for (int i = 0; i < rectangleWidth1; i++) {
+            for (int j = 0; j < rectangleHeight1; j++) {
+                System.out.print("* ");
+            }
+            System.out.print("\n");
+        }
+
+
+        System.out.println();
+
+
+        //Output empty rectangle
+        int rectangleWidth2 = 5;
+        int rectangleHeight2 = 10;
+        for (int i = 0; i < rectangleHeight2; i++) {
+            for (int j = 0; j < rectangleWidth2; j++) {
+                if ((i == 0) || (i == rectangleHeight2 - 1) || (j == 0) || (j == rectangleWidth2 - 1)) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.print("\n");
+        }
+    }
+
+
+}
