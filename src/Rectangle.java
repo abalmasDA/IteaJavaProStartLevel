5b1832510843f20340ff0b775b73c68858986d60
abalmasDA <abalmasit@gmail.com>
2023-04-25 21:47:20 +0300

    Commit28

--- src/Rectangle.java
+++ src/Rectangle.java
@@ -1,5 +1,8 @@
 public class Rectangle {
 
+    static String fillSymbols = "* ";
+    static String emptySymbols = "  ";
+
     public static void main(String[] args) {
 
         //Output filled rectangle
@@ -7,7 +10,7 @@ public class Rectangle {
         int rectangleWidth1 = 10;
         for (int i = 0; i < rectangleWidth1; i++) {
             for (int j = 0; j < rectangleHeight1; j++) {
-                System.out.print("* ");
+                System.out.print(fillSymbols + " ");
             }
             System.out.print("\n");
         }
@@ -21,10 +24,10 @@ public class Rectangle {
         int rectangleHeight2 = 10;
         for (int i = 0; i < rectangleHeight2; i++) {
             for (int j = 0; j < rectangleWidth2; j++) {
-                if ((i == 0) || (i == rectangleHeight2 - 1) || (j == 0) || (j == rectangleWidth2 - 1)) {
-                    System.out.print("* ");
+                if (i == 0 || i == rectangleHeight2 - 1 || j == 0 || j == rectangleWidth2 - 1) {
+                    System.out.print(fillSymbols);
                 } else {
-                    System.out.print("  ");
+                    System.out.print(emptySymbols );
                 }
             }
             System.out.print("\n");
