a9e97857e8cfeca1d37488b6751ec415fb62d3a1
abalmasDA <abalmasit@gmail.com>
2023-05-09 22:13:20 +0300

    Commit43

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,3 +1,8 @@
+/**
+ * Використовуючи IntelliJ IDEA, створіть клас Calculator. Створіть метод з ім'ям calculate,
+ * який приймає як параметри три цілих аргументи та виводить на екран середнє арифметичне значень аргументів.
+ */
+
 public class Calculator {
 
     public static double calculateAverage(int number1, int number2, int number3) {
