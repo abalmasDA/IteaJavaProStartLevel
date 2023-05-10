a236ae4b7b01b5e76b0fe61a7b96e78c7b2a1efd
abalmasDA <abalmasit@gmail.com>
2023-05-10 08:42:30 +0300

    Commit44

--- src/Calculator.java
+++ src/Calculator.java
@@ -6,15 +6,15 @@
 public class Calculator {
 
     public static double calculateAverage(int number1, int number2, int number3) {
-        double numberAverage = (number1 + number2 + number3) / 3;
+        double numberAverage = (number1 + number2 + number3) / 3.0;
         return numberAverage;
     }
 
     public static void main(String[] args) {
 
-        int number1 = 1;
-        int number2 = 2;
-        int number3 = 3;
+        int number1 = 2;
+        int number2 = 4;
+        int number3 = 4;
         System.out.println(calculateAverage(number1, number2, number3));
 
     }
